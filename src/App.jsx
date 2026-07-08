b1f57250825290c98f778b41d0ae55a33f9eaa12
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-07-08 16:54:16 -0400

    refactor: make resume object function scoped with lazy init

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
+import { useState } from 'react'
 import './App.css'
-
-/**
- * Shared state object that reflects what the user types into the form.
- * As the user enters data into the form, this object receives the most current state, and
- * reflects it in the resume template.
- */
-const resume = {
-  name: '',
-  email: '',
-  number: '',
-  education: [],
-  workExperience: []
-}
+import { ContactFields } from './components/form/ContactFields'
 
 function App() {
+  /**
+   * Shared state object that reflects what the user types into the form.
+   * As the user enters data into the form, this object receives the most current state, and
+   * reflects it in the resume template.
+   */
+  const [resume, setResume] = useState(() => ({
+    name: '',
+    email: '',
+    number: '',
+    education: [],
+    workExperience: []
+  }));
+
   return (
     <>
+     <ContactFields />
     </>
   )
 }
