1a6baa36dc8c72ce1951d7b99bdedcf3769de1fa
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-07-09 02:32:54 -0400

    feat: render remaining contact form components. Closes #3

--- src/components/form/ContactFields.jsx
+++ src/components/form/ContactFields.jsx
@@ -7,14 +7,28 @@ const contactFieldsConfig = [
     type: "text",
     placeholder: "John Smith",
     autoCompleteType: "name",
+  },
+  {
+    name: "email",
+    label: "Email Address",
+    type: "email",
+    placeholder: "email@example.com",
+    autoCompleteType: "email",
+  },
+  {
+    name: "number",
+    label: "Phone Number",
+    type: "tel",
+    placeholder: "+0 123 456 7890",
+    autoCompleteType: "tel",
   }
 ]
 
-export function ContactFields({onChange, value}) {
+export function ContactFields({onChange, resumeData}) {
   return (
     <fieldset>
       <legend>Contact Details</legend>
-      {contactFieldsConfig.map(field => <FormField key={field.name} value={value} onChange={onChange} {...field}/>)}
+      {contactFieldsConfig.map(field => <FormField key={field.name} value={resumeData[field.name]} onChange={onChange} {...field}/>)}
     </fieldset>
   )
 }
