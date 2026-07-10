448813703ea904fa53d3849a2f5c0ce6f3c4e020
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-07-10 12:14:01 -0400

    feat: create new EducationEntry component for each value in resume.education array

--- src/components/form/EducationFields.jsx
+++ src/components/form/EducationFields.jsx
@@ -5,8 +5,42 @@ export function EducationFields({onChange, resumeData, addField}) {
     <>
       <fieldset className="educationFields">
         <legend>Education</legend>
+        {resumeData.map(entry => <EducationEntry key={entry.id} id={entry.id} onChange={onChange}/> )}
         <button type="button" data-field-type="education" onClick={addField}>Add Education Field</button>
       </fieldset>
     </>
+  )
+}
+
+function EducationEntry({id, onChange}) {
+  return (
+    <div data-entry-id={id}>
+      <FormField 
+        name="schoolName" 
+        label="School Name" 
+        type="text" 
+        placeholder="Harvard University" 
+        onChange={onChange} 
+        entryId={id} 
+      />
+
+      <FormField 
+        name="degree" 
+        label="Degree / Certificate" 
+        type="text" 
+        placeholder="B.Sc. Computer Science" 
+        onChange={onChange}
+        entryId={id} 
+      />
+      
+      <FormField 
+        name="dateRange" 
+        label="Study Period" 
+        type="text" 
+        placeholder="2020.09 - 2024.04"
+        onChange={onChange}
+        entryId={id}
+      />
+    </div>
   )
 }
