09abc982deb8b169b91411b653c54a997846c03c
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-07-13 01:19:57 -0400

    refactor: create education inputs with a config object and control names via EDUCATION KEYS

--- src/components/form/EducationFields.jsx
+++ src/components/form/EducationFields.jsx
@@ -1,50 +1,55 @@
 import { FormField } from "./FormField";
-import { EDUCATION_KEYS, RESUME_KEYS } from "../../constants/resumeKeys";
+import { EDUCATION_KEYS } from "../../constants/resumeKeys";
+
+const educationFieldsConfig = [
+  {
+    name: EDUCATION_KEYS.SCHOOL_NAME,
+    label: "School Name",
+    type: "text",
+    placeholder: "Harvard University",
+  },
+  {
+    name: EDUCATION_KEYS.DEGREE,
+    label: "Degree / Certificate",
+    type: "text",
+    placeholder: "B.Sc. Computer Sciencce",
+  },
+  {
+    name: EDUCATION_KEYS.DATE_RANGE,
+    label: "Study Period",
+    type: "text",
+    placeholder: "2020.09 - 2024.04"
+  }
+]
 
 export function EducationFields({onChange, resumeData, addField, fieldType}) {
   return (
     <>
       <fieldset className="educationFields">
         <legend>Education</legend>
-        {resumeData.map(entry => <EducationEntry key={entry.id} id={entry.id} onChange={onChange} fieldType={fieldType}/> )}
-        <button type="button" data-field-type={RESUME_KEYS.EDUCATION} onClick={addField}>Add Education Field</button>
+        {resumeData.map(entry => <EducationEntry key={entry.id} entry={entry} onChange={onChange} fieldType={fieldType}/> )}
+        <button type="button" data-field-type={fieldType} onClick={addField}>Add Education Field</button>
       </fieldset>
     </>
   )
 }
 
-function EducationEntry({id, onChange, fieldType}) {
+function EducationEntry({entry, onChange, fieldType}) {
   return (
     <div>
-      <FormField 
-        name={EDUCATION_KEYS.SCHOOL_NAME}
-        label="School Name" 
-        type="text" 
-        placeholder="Harvard University" 
-        onChange={onChange} 
-        entryId={id}
-        fieldType={fieldType} 
-      />
-
-      <FormField 
-        name={EDUCATION_KEYS.DEGREE} 
-        label="Degree / Certificate" 
-        type="text" 
-        placeholder="B.Sc. Computer Science" 
-        onChange={onChange}
-        entryId={id}
-        fieldType={fieldType} 
-      />
-      
-      <FormField 
-        name={EDUCATION_KEYS.DATE_RANGE} 
-        label="Study Period" 
-        type="text" 
-        placeholder="2020.09 - 2024.04"
-        onChange={onChange}
-        entryId={id}
-        fieldType={fieldType}
-      />
+      {educationFieldsConfig.map(field => (
+        <FormField 
+          key={field.name}
+          value={entry[field.name]}
+          onChange={onChange}
+          name={field.name}
+          label={field.label}
+          type={field.type}
+          placeholder={field.placeholder}
+          entryId={entry.id}
+          fieldType={fieldType}
+        />
+      ))}
     </div>
   )
 }
