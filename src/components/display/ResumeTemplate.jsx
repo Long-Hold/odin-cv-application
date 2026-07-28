5d2c6fd809d8b255b4de1afcaefa92ab6298b551
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-07-27 20:36:08 -0400

    feat: call useFitToContainer and wrap resume template in a div (#24)

--- src/components/display/ResumeTemplate.jsx
+++ src/components/display/ResumeTemplate.jsx
@@ -2,29 +2,35 @@ import { EducationDetails } from "./EducationDetails";
 import { PersonalDetails } from "./PersonalDetails";
 import { SkillsDetails } from "./SkillsDetails";
 import { WorkExperienceDetails } from "./WorkExperienceDetails";
+import { useFitToContainer } from "../../hooks/useFitToContainer";
 import styles from "./styles/ResumeTemplate.module.css";
+import { useRef } from "react";
 
 export function ResumeTemplate({resumeObject}) {
+  const resumeContainerRef = useRef(null);
+  useFitToContainer(resumeContainerRef);
   return (
-    <section className={styles.resumeTemplate}>
-      <PersonalDetails 
-        name={resumeObject.name}
-        email={resumeObject.email}
-        number={resumeObject.number}
-        summary={resumeObject.summary}
-      />
+    <div ref={resumeContainerRef} className={styles.resumeContainer}>
+      <section className={styles.resumeTemplate}>
+        <PersonalDetails 
+          name={resumeObject.name}
+          email={resumeObject.email}
+          number={resumeObject.number}
+          summary={resumeObject.summary}
+        />
 
-      <EducationDetails 
-        educationArray={resumeObject.education}
-      />
+        <EducationDetails 
+          educationArray={resumeObject.education}
+        />
 
-      <WorkExperienceDetails 
-        workExperienceArray={resumeObject.workExperience}
-      />
+        <WorkExperienceDetails 
+          workExperienceArray={resumeObject.workExperience}
+        />
 
-      <SkillsDetails 
-        skillsArray={resumeObject.skills}
-      />
-    </section>
+        <SkillsDetails 
+          skillsArray={resumeObject.skills}
+        />
+      </section>
+    </div>
   )
 }
