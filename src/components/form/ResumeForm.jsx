9e05b95cd3b269780db95cece317190ef52939e7
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-08-13 18:25:40 -0400

    feat: create new view / hide resume button using state for mobile screens (#39)

--- src/components/form/ResumeForm.jsx
+++ src/components/form/ResumeForm.jsx
@@ -3,53 +3,56 @@ import { EducationFields } from "./EducationFields";
 import { WorkExperienceFields } from "./WorkExperienceFields";
 import { SkillsFields } from "./SkillsFields";
 import "./styles/formStyles.css"
+import { useState } from "react";
 
 export function ResumeForm({resumeObject, handleContactChange, handleEntryFieldChange, addNewResumeData, removeResumeEntry}) {
+  const [isFormOpen, setFormState] = useState(true);
+  const toggleForm = () => {
+    setFormState((prevState) => !prevState);
+  };
   return (
-    <section className="resumeFormSection">
-      <button 
-        type="button" 
-        className="toggleHideFormBtn" 
-        onClick={() => document.querySelector('.resumeDetailsForm').classList.remove('hide')}
-      >edit resume</button>
+    <>
+      <header className="formVisibilityControls">
+        <button
+          type="button"
+          className="toggleHideFormBtn"
+          onClick={() => toggleForm()}
+          aria-expanded={isFormOpen}
+        >{isFormOpen ? "view resume" : "edit resume"}</button>
+      </header>
+      <section className={`resumeFormSection${isFormOpen ? "" : " hide"}`}>
+        <form 
+          className="resumeDetailsForm"
+          onSubmit={(event) => event.preventDefault}
+          noValidate
+        >
+          <PersonalFields 
+            onChange={handleContactChange} 
+            resumeData={resumeObject} 
+          />
 
-      <form 
-        className="resumeDetailsForm"
-        onSubmit={(event) => event.preventDefault}
-        noValidate
-      >
-        <PersonalFields 
-          onChange={handleContactChange} 
-          resumeData={resumeObject} 
-        />
+          <SkillsFields 
+            onChange={handleEntryFieldChange}
+            resumeData={resumeObject.skills}
+            addField={addNewResumeData}
+            removeEntry={removeResumeEntry}
+          />
 
-        <SkillsFields 
-          onChange={handleEntryFieldChange}
-          resumeData={resumeObject.skills}
-          addField={addNewResumeData}
-          removeEntry={removeResumeEntry}
-        />
+          <WorkExperienceFields
+            onChange={handleEntryFieldChange} 
+            resumeData={resumeObject.workExperience} 
+            addField={addNewResumeData} 
+            removeEntry={removeResumeEntry}
+          />
 
-        <WorkExperienceFields
-          onChange={handleEntryFieldChange} 
-          resumeData={resumeObject.workExperience} 
-          addField={addNewResumeData} 
-          removeEntry={removeResumeEntry}
-        />
-
-        <EducationFields 
-          onChange={handleEntryFieldChange} 
-          resumeData={resumeObject.education} 
-          addField={addNewResumeData} 
-          removeEntry={removeResumeEntry}
-        />
-
-        <button 
-          type="button" 
-          className="toggleHideFormBtn" 
-          onClick={() => document.querySelector('.resumeDetailsForm').classList.add('hide')}
-        >view resume</button>
-      </form>
-    </section>
+          <EducationFields 
+            onChange={handleEntryFieldChange} 
+            resumeData={resumeObject.education} 
+            addField={addNewResumeData} 
+            removeEntry={removeResumeEntry}
+          />
+        </form>
+      </section>
+    </>
   )
 }
