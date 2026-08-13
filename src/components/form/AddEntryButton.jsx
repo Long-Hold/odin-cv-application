43f72d1f8076cdf819f2a331e47c79d24065eaa2
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-08-13 13:07:50 -0400

    feat: create AddEntryButton component that adds a new entry to the form

---
+++ src/components/form/AddEntryButton.jsx
@@ -0,0 +1,9 @@
+export function AddEntryButton({addField, fieldKey, text}) {
+  return (
+    <button
+      type="button"
+      className="addEntryBtn"
+      onClick={() => addField(fieldKey)}
+    >{text}</button>
+  )
+}
