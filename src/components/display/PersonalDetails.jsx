a899f1e9ef6c37880686f8eb9d2adf7ce259379a
Matthew <102175343+Long-Hold@users.noreply.github.com>
2026-08-25 17:29:47 -0400

    feat: add hook and custom css for resize function (#40)

--- src/components/display/PersonalDetails.jsx
+++ src/components/display/PersonalDetails.jsx
@@ -1,9 +1,14 @@
+import { useRef } from "react";
+import { useFitText } from "../../hooks/resizeTextFont";
 import "./styles/ResumeTemplate.css";
 
 export function PersonalDetails({name, email, number, summary}) {
+  const nameRef = useRef(null);
+  useFitText(nameRef, name, 22);
+
   return (
     <section className="personalDetails">
-      <h1>{name}</h1>
+      <h1 ref={nameRef}>{name}</h1>
       <p>{email}</p>
       <p>{number}</p>
       <p className="multiLine">{summary}</p>
