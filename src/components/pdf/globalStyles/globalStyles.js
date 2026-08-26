d45870571ab9d41fc1df090817c42abc12cac2eb
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-08-26 16:45:07 -0400

    feat: add first set of global pdf style rules (#47)

---
+++ src/components/pdf/globalStyles/globalStyles.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from "@react-pdf/renderer";
+
+export const GLOBAL_STYLES = StyleSheet.create({
+  detailSection: {
+    flexDirection: 'column',
+    flexWrap: 'nowrap',
+    gap: '16px',
+  },
+});
