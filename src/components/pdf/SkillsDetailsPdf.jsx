d53217a1469e2431be4d8bc00bee796c2f5cab86
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-08-31 14:20:38 -0400

    feat: create Skills section (#47)

---
+++ src/components/pdf/SkillsDetailsPdf.jsx
@@ -0,0 +1,23 @@
+import { View, Text, StyleSheet } from "@react-pdf/renderer";
+import { GLOBAL_STYLES } from "./globalStyles/globalStyles";
+
+const styles = StyleSheet.create({
+  list: {
+    flexDirection: 'column',
+    gap: '8px',
+  }
+})
+
+export function SkillsDetailsPdf({skillsArray}) {
+  return (
+    <View style={GLOBAL_STYLES.detailSection}>
+      <Text>SKILLS</Text>
+
+      <View style={styles.list}>
+        {skillsArray.map(entry => 
+          <Text key={entry.id}>{entry.skill}</Text>
+        )}
+      </View>
+    </View>
+  )
+}
